2a2c51819af5d46e09770d1f881cf54d0cf59a1a
Alexander Rovang <wolf_vii@yahoo.com>
2021-06-18 15:06:29 -0400

    adds multiple cards to show

--- src/components/Draw.js
+++ src/components/Draw.js
@@ -5,12 +5,20 @@ import InterpretationsContainer from '../containers/InterpretationsContainer'
 const Draw = props =>{ 
 
   let draw = props.draws.draws[props.match.params.id - 1]
-
+// debugger
     return(
-      <div className="Field">
+      <div>
         Layout: {draw ? draw.layout: null}<br></br>
-        {draw && (props.cards.length > 0) ? <img src={props.cards[draw.card - 1].image}/> : null}<br></br>
-        Card Name: {draw  && (props.cards.length > 0) ? <Link to={`/cards/${props.cards[draw.card - 2].id}`}>{props.cards[draw.card - 1].name}</Link> : null}
+
+        {draw && (props.cards.length > 0) ? 
+       
+       <div>{draw.card_ids.map(card_id => <div key={card_id.id}> <img src={props.cards[card_id - 1].image}/></div>)}</div>
+        : null}<br></br> 
+
+        {/* 
+        <Link to={`/cards/${props.cards[draw.card - 2].id}`}>{props.cards[draw.card - 1].name}</Link>
+         */}
+        
         <InterpretationsContainer draw={draw} />
       </div>
     )
@@ -18,4 +26,3 @@ const Draw = props =>{
   export default Draw
 
 
-  
