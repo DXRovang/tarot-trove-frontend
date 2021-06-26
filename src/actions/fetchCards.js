f13af28537b561d4f353ec33c568cb77823fbeaf
Alexander Rovang <wolf_vii@yahoo.com>
2021-06-26 18:32:40 -0400

    changing fetch

--- src/actions/fetchCards.js
+++ src/actions/fetchCards.js
@@ -1,6 +1,6 @@
 export function fetchCards(){
   return (dispatch) => {
-    fetch('http://127.0.0.1:3000/api/v1/cards')
+    fetch('http://tarot-trove-backend.herokuapp.com/api/v1/cards')
     .then(r=>r.json())
     .then(cards=> dispatch({
       type: "FETCH_CARDS", 
