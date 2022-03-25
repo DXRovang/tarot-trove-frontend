6132142c848d842f790baa66ebbb9deadc2ea233
Alexander Rovang <DXRovang@gmail.com>
2022-03-25 08:40:44 -0400

    refactors major arcana & deletes deprecated code

--- src/components/MajorArcana.js
+++ src/components/MajorArcana.js
@@ -1,77 +1,57 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 import {
-  Card, Container, Row, Col, CardImg, CardTitle,
-  CardSubtitle, CardBody
-} from 'reactstrap';
+  Card,
+  Container,
+  Row,
+  Col,
+  CardImg,
+  CardTitle,
+  CardSubtitle,
+  CardBody,
+} from "reactstrap";
 
-const MajorArcana = props =>{
-  return(
+const ColumnTemplate = (card) => {
+  return (
+    <Col>
+      <Card key={card.id}>
+        <Link to={`/cards/${card.id - 1}`}>
+          <CardImg top width="100%" src={card.image} />
+          <CardBody>
+            <CardTitle className="CardTitle" tag="h5">
+              {card.name}
+            </CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">
+              {card.id - 1}
+            </CardSubtitle>
+          </CardBody>
+        </Link>
+      </Card>
+    </Col>
+  );
+};
 
+const MajorArcana = (props) => {
+  return (
     <Container>
-            <Row>
-        <div>{props.cards.slice(0,1).map(card => 
-        <Col>
-          <Card key={card.id}>
-            <Link to={`/cards/${card.id - 1}`}>
-              <CardImg top width="100%" src={card.image} />
-              <CardBody>          
-                <CardTitle className="CardTitle"tag="h5">{card.name}</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">{card.id - 1}</CardSubtitle>
-              </CardBody>
-            </Link>
-          </Card>
-        </Col>
-       )}</div>
+      <Row>
+        <div>{props.cards.slice(0, 1).map((card) => ColumnTemplate(card))}</div>
       </Row>
       <Row>
-        <div>{props.cards.slice(1,8).map(card => 
-        <Col>
-          <Card key={card.id}>
-            <Link to={`/cards/${card.id - 1}`}>
-              <CardImg top width="100%" src={card.image} />
-              <CardBody>          
-                <CardTitle className="CardTitle" tag="h5">{card.name}</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">{card.id - 1}</CardSubtitle>
-              </CardBody>
-            </Link>
-          </Card>
-        </Col>
-       )}</div>
+        <div>{props.cards.slice(1, 8).map((card) => ColumnTemplate(card))}</div>
       </Row>
       <Row>
-        <div>{props.cards.slice(8,15).map(card => 
-        <Col>
-          <Card key={card.id}>
-            <Link to={`/cards/${card.id - 1}`}>
-              <CardImg top width="100%" src={card.image} />
-              <CardBody>          
-                <CardTitle className="CardTitle" tag="h5">{card.name}</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">{card.id - 1}</CardSubtitle>    
-              </CardBody>
-            </Link>
-          </Card>
-        </Col>
-       )}</div>
+        <div>
+          {props.cards.slice(8, 15).map((card) => ColumnTemplate(card))}
+        </div>
       </Row>
       <Row>
-        <div>{props.cards.slice(15,22).map(card => 
-        <Col>
-          <Card key={card.id}>
-            <Link to={process.env.PUBLIC_URL +`/cards/${card.id - 1}`}>
-              <CardImg top width="100%" src={card.image} />
-              <CardBody>          
-                <CardTitle className="CardTitle" tag="h5">{card.name}</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">{card.id - 1}</CardSubtitle>
-              </CardBody>
-            </Link>
-          </Card>
-        </Col>
-       )}</div>
+        <div>
+          {props.cards.slice(15, 22).map((card) => ColumnTemplate(card))}
+        </div>
       </Row>
-  
     </Container>
-  )
-}
+  );
+};
 
-export default MajorArcana
+export default MajorArcana;
