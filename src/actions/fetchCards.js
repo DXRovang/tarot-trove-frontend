e716886dc76be08cfdd308595fa930c1a994e8e8
Alexander Rovang <DXRovang@gmail.com>
2023-01-10 14:37:41 -0500

    updates api endpoint

--- src/actions/fetchCards.js
+++ src/actions/fetchCards.js
@@ -1,7 +1,7 @@
 export function fetchCards(){
   return (dispatch) => {
-    fetch('https://tarot-trove-backend.herokuapp.com/api/v1/cards')
-    // fetch('http://127.0.0.1:3000/api/v1/cards')
+    // fetch('https://tarot-trove-backend.herokuapp.com/api/v1/cards')
+    fetch('http://127.0.0.1:3000/api/v1/cards')
     .then(r=>r.json())
     .then(cards=> dispatch({
       type: "FETCH_CARDS", 
